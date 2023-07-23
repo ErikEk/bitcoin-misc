a0070d16295a87887dde76d4f7d8dd894ecd78ca
erik <erik.ek@gmail.com>
2023-07-23 12:35:52 +0200

    generate_key_pair

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
-import bitcoinlib
-import hashlib
-import base58
-import codecs
-import ecdsa
+from bitcoinutils.setup import setup
+from bitcoinutils.keys import P2pkhAddress, PrivateKey, PublicKey
 
-#privKey1, pubKey1 = generate_key_pair(sha256(b'key0'))
-#privKey2, pubKey2 = generate_key_pair(sha256(b'key1'))
-#privKey3, pubKey3 = generate_key_pair(sha256(b'key2'))
+def main():
+    # always remember to setup the network
+    setup('mainnet')
 
-from bitcoinlib.wallets import Wallet
-w = Wallet.create('Wallet1')
-w.get_key().address
+    # create a private key (deterministically)
+    priv = PrivateKey(secret_exponent = 1)
+
+    # compressed is the default
+    print("\nPrivate key WIF:", priv.to_wif(compressed=True))
+
+    # could also instantiate from existing WIF key
+    #priv = PrivateKey.from_wif('KwDiBf89qGgbjEhKnhxjUh7LrciVRzI3qYjgd9m7Rfu73SvHnOwn')
+
+    # get the public key
+    pub = priv.get_public_key()
+
+    # compressed is the default
+    print("Public key:", pub.to_hex(compressed=True))
+
+    # get address from public key
+    address = pub.get_address()
+
+    # print the address and hash160 - default is compressed address
+    print("Address:", address.to_string())
+    print("Hash160:", address.to_hash160())
+
+    print("\n--------------------------------------\n")
+
+    # sign a message with the private key and verify it
+    message = "The test!"
+    signature = priv.sign_message(message)
+    print("The message to sign:", message)
+    print("The signature is:", signature)
+
+    if PublicKey.verify_message(address.to_string(), signature, message):
+        print("The signature is valid!")
+    else:
+        print("The signature is NOT valid!")
+
+
+if __name__ == "__main__":
+    main()
