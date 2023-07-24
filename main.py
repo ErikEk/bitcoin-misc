141689e5e03af089b0ba09505b5484ee3f08ee0c
erik <erik.ek@gmail.com>
2023-07-24 12:23:49 +0200

    new

--- main.py
+++ main.py
@@ -1,44 +1,101 @@
+
 from bitcoinutils.setup import setup
-from bitcoinutils.keys import P2pkhAddress, PrivateKey, PublicKey
+from bitcoinutils.utils import to_satoshis
+from bitcoinutils.script import Script
+from bitcoinutils.transactions import Transaction, TxInput, TxOutput, TxWitnessInput
+from bitcoinutils.keys import PrivateKey
+from bitcoinutils.hdwallet import HDWallet
 
 def main():
     # always remember to setup the network
     setup('testnet')
 
-    # create a private key (deterministically)
-    priv = PrivateKey(secret_exponent = 1)
+    # Keys are hard-coded in the example for simplicity but it is very bad
+    # practice. Normally you would acquire them from env variables, db, etc.
 
-    # compressed is the default
-    print("\nPrivate key WIF:", priv.to_wif(compressed=True))
+    #######################
+    # Construct the input #
+    #######################
 
-    # could also instantiate from existing WIF
-    #priv = PrivateKey.from_wif('KwDiBf89qGgbjEhKnhxjUh7LrciVRzI3qYjgd9m7Rfu73SvHnOwn')
+    # get an HDWallet wrapper object by extended private key and path
+    xprivkey = "tprv8ZgxMBicQKsPdQR9RuHpGGxSnNq8Jr3X4WnT6Nf2eq7FajuXyBep5KWYpYEixxx5XdTm1Ntpe84f3cVcF7mZZ7mPkntaFXLGJD2tS7YJkWU"
+    path = "m/86'/1'/0'/0/6"
+    hdw = HDWallet(xprivkey, path)
+    internal_priv = hdw.get_private_key()
+    print('From Private key:', internal_priv.to_wif())
 
-    # get the public key
-    pub = priv.get_public_key()
+    internal_pub = internal_priv.get_public_key()
+    print('From Public key:', internal_pub.to_hex())
 
-    # compressed is the default
-    print("Public key:", pub.to_hex(compressed=True))
+    from_address = internal_pub.get_taproot_address()
+    print('From Taproot address:', from_address.to_string())
 
-    # get address from public key
-    address = pub.get_address()
+    # UTXO of from address
+    txid = '67e8c015625279f2d4268a7b15e8a6feef39a86ed4f5c14acbd260f612b8c44a'
+    vout = 1
 
-    # print the address and hash160 - default is compressed address
-    print("Address:", address.to_string())
-    print("Hash160:", address.to_hash160())
+    # create transaction input from tx id of UTXO
+    tx_in = TxInput(txid, vout)
 
-    print("\n--------------------------------------\n")
+    # all amounts are needed to sign a taproot input
+    # (depending on sighash)
+    amount = to_satoshis(0.00009658)
+    amounts = [ amount ]
 
-    # sign a message with the private key and verify it
-    message = "The test!"
-    signature = priv.sign_message(message)
-    print("The message to sign:", message)
-    print("The signature is:", signature)
+    # all scriptPubKeys (in hex) are needed to sign a taproot input 
+    # (depending on sighash but always of the spend input)
+    scriptPubkey = from_address.to_script_pub_key()
+    utxos_scriptPubkeys = [ scriptPubkey ]
 
-    if PublicKey.verify_message(address.to_string(), signature, message):
-        print("The signature is valid!")
-    else:
-        print("The signature is NOT valid!")
+    ########################
+    # Construct the output #
+    ########################
+
+    hdw.from_path("m/86'/1'/0'/0/7")
+    to_priv = hdw.get_private_key()
+    print('To Private key', to_priv.to_wif())
+    to_pub = to_priv.get_public_key()
+    print('To Public key', to_pub.to_hex())
+
+    # taproot script is a simple P2PK with the following keys
+
+    # tapleaf script p2pk script
+    privkey_tr_script = PrivateKey('cQwzrJyTNWbEwhPEmQ3Qoo4jSfHdHEtdbL4kNBgHUKhirgzcQw7G')
+    pubkey_tr_script = privkey_tr_script.get_public_key()
+    tr_script_p2pk = Script([pubkey_tr_script.to_x_only_hex(), 'OP_CHECKSIG'])
+
+    # taproot script path address
+    to_address = to_pub.get_taproot_address([ [tr_script_p2pk] ])
+    print('To Taproot script address', to_address.to_string())
+
+    # create transaction output
+    tx_out = TxOutput(to_satoshis(0.00009), to_address.to_script_pub_key())
+
+    # create transaction without change output - if at least a single input is
+    # segwit we need to set has_segwit=True
+    tx = Transaction([tx_in], [tx_out], has_segwit=True)
+
+    print("\nRaw transaction:\n" + tx.serialize())
+
+    print('\ntxid: ' + tx.get_txid())
+    print('\ntxwid: ' + tx.get_wtxid())
+
+    # sign taproot input
+    # to create the digest message to sign in taproot we need to
+    # pass all the utxos' scriptPubKeys and their amounts
+    sig = internal_priv.sign_taproot_input(tx, 0, utxos_scriptPubkeys, amounts)
+
+    tx.witnesses.append( TxWitnessInput([ sig ]) )
+
+    # print raw signed transaction ready to be broadcasted
+    print("\nRaw signed transaction:\n" + tx.serialize())
+
+    print("\nTxId:", tx.get_txid())
+    print("\nTxwId:", tx.get_wtxid())
+
+    print("\nSize:", tx.get_size())
+    print("\nvSize:", tx.get_vsize())
+
 
 if __name__ == "__main__":
     main()
